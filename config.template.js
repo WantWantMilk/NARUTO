23a6579b5ba32df1384b70cce77a910bb5f7ca87
WantWantMilk <li.bojin@outlook.com>
2026-05-22 22:50:17 +0800

    删除 config.template.js

--- config.template.js
+++
@@ -1,6 +0,0 @@
-// config.template.js
-// 部署时会被 GitHub Actions 自动替换为真实密钥
-window.SUPABASE_CONFIG = {
-  url: 'https://mbyhzadgigkquuoxuacr.supabase.co',
-  anonKey: 'sb_publishable_9KJoJb40Na7Xey0pOgEhvw_JMQdfUUi'
-};
